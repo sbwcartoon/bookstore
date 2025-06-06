ad97ea43bc1c2dd477d8fe2dca024325b9520cc6
sbwcartoon <sbwcartoon@gmail.com>
2025-06-07 05:54:32 +0900

    feat: 책 상세 정보 조회 컨트롤러 추가

--- src/main/kotlin/com/rgt/bookstore/book/adapter/in/web/BookController.kt
+++ src/main/kotlin/com/rgt/bookstore/book/adapter/in/web/BookController.kt
@@ -1,20 +1,28 @@
 package com.rgt.bookstore.book.adapter.`in`.web
 
+import com.rgt.bookstore.book.adapter.`in`.dto.BookDetail
 import com.rgt.bookstore.book.adapter.`in`.dto.CreateBookRequest
 import com.rgt.bookstore.book.adapter.`in`.dto.CreateBookResponse
 import com.rgt.bookstore.book.application.port.`in`.CreateBookUseCase
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.rgt.bookstore.book.application.port.`in`.GetBookUseCase
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/books")
-class BookController(private val createBookUseCase: CreateBookUseCase) {
+class BookController(
+    private val createBookUseCase: CreateBookUseCase,
+    private val getBookUseCase: GetBookUseCase,
+) {
 
     @PostMapping
     fun create(@RequestBody createBookRequest: CreateBookRequest): CreateBookResponse {
         val createdBook = createBookUseCase.execute(createBookRequest.toCommand())
         return CreateBookResponse.from(createdBook)
+    }
+
+    @GetMapping("/{id}")
+    fun getBook(@PathVariable id: String): BookDetail {
+        val book = getBookUseCase.execute(id)
+        return BookDetail.from(book)
     }
 }
