423f1d75e1f270642c5c4fc5ab611fe1d212b6d3
sbwcartoon <sbwcartoon@gmail.com>
2025-06-07 16:28:52 +0900

    test: 책 정보 수정 통합 테스트 추가

---
+++ src/test/kotlin/com/rgt/bookstore/book/integration/UpdateBookSucceedTest.kt
@@ -0,0 +1,64 @@
+package com.rgt.bookstore.book.integration
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.rgt.bookstore.book.adapter.`in`.dto.CreateBookRequest
+import com.rgt.bookstore.book.adapter.`in`.dto.CreateBookResponse
+import com.rgt.bookstore.book.adapter.`in`.dto.UpdateBookRequest
+import com.rgt.bookstore.book.adapter.out.persistence.repository.BookJpaRepository
+import com.rgt.bookstore.book.integration.config.IntegrationTest
+import io.kotest.core.spec.style.BehaviorSpec
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
+import org.springframework.test.web.servlet.put
+
+@IntegrationTest
+@AutoConfigureMockMvc
+class UpdateBookSucceedTest(
+    private val mockMvc: MockMvc,
+    private val objectMapper: ObjectMapper,
+    private val jpaRepository: BookJpaRepository,
+) : BehaviorSpec({
+
+    beforeSpec {
+        jpaRepository.deleteAll()
+    }
+
+    Given("기존에 존재하는 책 정보 수정 요청을 할 때") {
+        val existing = CreateBookRequest(
+            title = "title",
+            author = "author",
+            price = 1,
+            quantity = 1,
+        )
+
+        val createResponseString = mockMvc.post("/api/books") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(existing)
+        }.andExpect {
+            status { isOk() }
+        }.andReturn().response.contentAsString
+
+        val existingDto = objectMapper.readValue(createResponseString, CreateBookResponse::class.java)
+
+        When("조건을 만족하면") {
+            Then("수정된다") {
+                val updateRequest = UpdateBookRequest(
+                    id = existingDto.id,
+                    title = "title",
+                    author = "author",
+                    price = 1,
+                    quantity = 1,
+                )
+
+                mockMvc.put("/api/books/${existingDto.id}") {
+                    contentType = MediaType.APPLICATION_JSON
+                    content = objectMapper.writeValueAsString(updateRequest)
+                }.andExpect {
+                    status { isOk() }
+                }
+            }
+        }
+    }
+})
